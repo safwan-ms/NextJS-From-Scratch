1d1c3c9e3f55d069c2ad2013aacb4377cb936c80
safwan-ms <safwan.anasms@gmail.com>
2025-03-31 23:21:45 +0530

    Experiment with Next.js Not-found.tsx

---
+++ 10. Not Found/1. Live Coding/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <h2 className="text-red-500 text-3xl font-bold">
+        Sorry this page does not exists
+      </h2>
+    </div>
+  );
+};
+export default NotFound;
