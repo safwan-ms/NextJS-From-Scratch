576181f3e954e07e0859076e1c6f7c17fc892595
safwan-ms <safwan.anasms@gmail.com>
2025-04-01 23:35:53 +0530

    HTTP DELETE Method

---
+++ 15. Backend/5. DELETE/movies/db.ts
@@ -0,0 +1,36 @@
+export const movies = [
+  {
+    id: 1,
+    name: "John Wick",
+    ratings: 1,
+    genre: "Action",
+  },
+
+  {
+    id: 2,
+    name: "The Shawshank Redemption",
+    ratings: 2,
+    genre: "Drama",
+  },
+
+  {
+    id: 3,
+    name: "The Godfather",
+    ratings: 3,
+    genre: "Drama",
+  },
+
+  {
+    id: 4,
+    name: "The Dark Knight",
+    ratings: 4,
+    genre: "Action",
+  },
+
+  {
+    id: 5,
+    name: "Inception",
+    ratings: 5,
+    genre: "Action",
+  },
+];
